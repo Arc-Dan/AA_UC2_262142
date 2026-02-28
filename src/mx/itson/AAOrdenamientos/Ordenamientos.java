00e73a959fb3441ab2d1d277f0443130cdebeca0
dag13 <dag13@L-15-cs-3>
2026-02-27 17:53:29 -0800

    Creación del proyecto: "AAOrdenamientos". Creación de Paquete: "mx.itson.AAOrdenamientos" que contiene la clase: "Ordenamientos" con función Main.

---
+++ src/mx/itson/AAOrdenamientos/Ordenamientos.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.AAOrdenamientos;
+
+/**
+ *
+ * @author dag13
+ */
+public class Ordenamientos {
+    public static void main(String[] args) {
+        
+    }
+}
