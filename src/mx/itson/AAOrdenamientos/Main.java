4dafa150209b3af008bbc639e10216aaf07e46a9
dag13 <dag13@L-15-cs-3>
2026-03-08 15:32:43 -0800

    Creación de método: "burbuja" en la clase "Ordenamientos". Creación de clase principal con método "main" y declaración de arreglo.

---
+++ src/mx/itson/AAOrdenamientos/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.AAOrdenamientos;
+
+/**
+ *
+ * @author dag13
+ */
+public class Main {
+    public static void main(String[] args) {
+        //Se crea una instancia de la clase "Ordenamientos":
+        Ordenamientos ordenamientos = new Ordenamientos();
+        
+        //Se declara el arreglo a ordenar
+        int[] arreglo = {6,3,8,9,4};
+        
+        //Se utiliza el método de instancia del algoritmo deseado y se da como argumento el arreglo
+        ordenamientos.burbuja(arreglo);
+        
+        //Se imprime el arreglo ordenado
+        for (int e : arreglo) {
+            System.out.print(e + " ");
+        }
+        System.out.println("");
+    }
+}
--- src/mx/itson/AAOrdenamientos/Ordenamientos.java
+++ src/mx/itson/AAOrdenamientos/Ordenamientos.java
@@ -9,7 +9,33 @@ package mx.itson.AAOrdenamientos;
  * @author dag13
  */
 public class Ordenamientos {
-    public static void main(String[] args) {
+    
+    //Método para el ordenamiento de Burbuja
+    public static int[] burbuja(int[] arreglo){
+        // Se declara variable 'n' para almacenar el tamaño del arreglo
+        int n = arreglo.length;
         
+        //Inicio del algoritmo
+        //Se iterará el arreglo desde elemento con index 0 hasta el último elemento
+        for(int i = 0; i < n-1; i++){
+            
+            /* Se iterará el arreglo para cada elemento del mismo, comenzando desde elemento
+               con index 0, hasta el último, menos la cantidad de ciclos externos completados */
+            for(int j = 0; j < n-1-i; j++){
+                
+                //Condicional para comparar si valor del elemento actual es mayor a valor del elemento siguiente
+                if(arreglo[j] > arreglo[j+1]){
+                    //Se declara variable auxiliar para almacenar valor mayor
+                    int aux = arreglo[j];
+                    //Se asigna valor de elemento menor al elemento de la posición actual
+                    arreglo[j] = arreglo[j+1];
+                    //Se asigna valor del auxiliar (mayor valor) al menor (se intercambian los elementos)
+                    arreglo[j+1] = aux;
+                }
+            }
+        }
+        //Se retorna el arreglo ordenado
+        return arreglo;
     }
+    
 }
