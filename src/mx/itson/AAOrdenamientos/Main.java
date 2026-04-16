373cb18db13c6fd75a49849632e5a2afdfe93eb5
dag13 <dag13@L-15-cs-3>
2026-04-16 14:35:15 -0800

    Se creó clase "Busqueda" y se agregó un bloque "switch" para elegir el tipo de algoritmo para ordenar el arreglo en la clase "Main".

--- src/mx/itson/AAOrdenamientos/Main.java
+++ src/mx/itson/AAOrdenamientos/Main.java
@@ -4,6 +4,8 @@
  */
 package mx.itson.AAOrdenamientos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author dag13
@@ -16,17 +18,28 @@ public class Main {
         //Se declara el arreglo a ordenar
         int[] arreglo = {2,-8,0,2};
         
+        Scanner scanner = new Scanner(System.in);
+        //Switch para elegir algoritmo de ordenamiento
+        System.out.println("Elija el algoritmo de ordenamiento:\n1.- Burbuja\n2.- Selección\n3.- Inserción\n4.- QuickSort\n5.- Merge Sort\nOpción: ");
+        
+        int opcion = scanner.nextInt();
+        
         //Se imprime el arreglo desordenado
         System.out.println("\nArreglo: " + java.util.Arrays.toString(arreglo));
         
-        //Se utiliza el método de instancia del algoritmo deseado y se da como argumento el arreglo
-        //ordenamientos.burbuja(arreglo);
-        //ordenamientos.seleccion(arreglo);
-        //ordenamientos.insercion(arreglo);
-        //ordenamientos.quickSort(arreglo, 0, arreglo.length-1);
-        ordenamientos.mergeSort(arreglo, 0, arreglo.length-1);
+        // Switch para ejecutar el algoritmo elegido
+        switch(opcion) {
+            case 1: ordenamientos.burbuja(arreglo); break;
+            case 2: ordenamientos.seleccion(arreglo); break;
+            case 3: ordenamientos.insercion(arreglo); break;
+            case 4: ordenamientos.quickSort(arreglo, 0, arreglo.length-1); break;
+            case 5: ordenamientos.mergeSort(arreglo, 0, arreglo.length-1); break;
+            default: System.out.println("Opción no válida.");
+        }
         
         //Se imprime el arreglo ordenado
         System.out.println("\nArreglo ordenado: " + java.util.Arrays.toString(arreglo) + "\n");
+        
+        scanner.close();
     }
 }
